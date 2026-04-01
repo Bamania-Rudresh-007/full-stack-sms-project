167264a1c8edb1623c0792059eb55c110eaf2975
Rudresh <bamaniarudresh2008@gmail.com>
2026-04-01 08:02:14 +0530

    install and use cors to allow cros origin

--- sms-backend/server.js
+++ sms-backend/server.js
@@ -3,10 +3,12 @@ import express from "express"
 import connectDb from "./src/db/connect.db.js"
 import authRouter from "./src/routes/auth.routes.js"
 import operationsRouter from "./src/routes/home.routes.js"
+import cors from "cors"
 
 const app = express()
 const port = process.env.PORT
 
+app.use(cors())
 app.use(express.json())
 
 app.get("/", (req, res) => {
